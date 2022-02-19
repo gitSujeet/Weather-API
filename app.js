ff5052187ad5cb16f85a2ecfd0a3f1b0b73f3641
Sujeet Kumar <92161069+gitSujeet@users.noreply.github.com>
2022-02-19 19:22:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
     const query = req.body.cityName;
-    const apiKey="494427679882aa9b53471443128af3d5";
+    const apiKey="(apiKey";
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units="+ unit +"&appid="+ apiKey;
     https.get(url, function(response){
